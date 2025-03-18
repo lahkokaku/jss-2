e3496e2fa4b19c36ad67d7f79a6c57cbd53c315a
Jose Stephen <jose.satrya@gmail.com>
2025-03-18 11:45:29 +0700

    chore: add featuredProjects data

---
+++ src/types/data/FeaturedProjects.ts
@@ -0,0 +1,23 @@
+export default interface FeaturedProjects {
+  title: string,
+  slug: string,
+  techStack: string[],
+  shortDescription: string,
+  description: {
+    introduction: string,
+    role?: string
+  },
+  codeDisclaimer: boolean,
+  linkCode: [
+    {
+      type: string,
+      link: string
+    }
+  ] | null,
+  linkWeb: string | null,
+  thumbnail: string,
+  interfaces: {
+    description: string,
+    link: string
+  }[] | null
+}
