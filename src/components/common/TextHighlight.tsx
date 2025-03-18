0de3dcf98fe86f7604fee2ee55c7f17b2466bb8c
Jose Stephen <jose.satrya@gmail.com>
2025-03-18 15:54:11 +0700

    feat: remove spinning gradient from TextHighlight as it was causing TBT to shoot up

--- src/components/common/TextHighlight.tsx
+++ src/components/common/TextHighlight.tsx
@@ -1,26 +1,15 @@
 import TextHighlightProps from '@/types/components/common/TextHighlightProps'
 import clsx from 'clsx'
-import { FC, useEffect, useRef, useState } from 'react'
+import { FC, useRef } from 'react'
 
 const TextHighlight: FC<TextHighlightProps> = ({ children, className }) => {
   const ref = useRef<HTMLElement | null>(null)
-  const [degree, setDegree] = useState<number>(0)
-
-  useEffect((): (() => void) => {
-    const interval = setInterval((): void => {
-      setDegree((degree + 20) % 360)
-      if (ref.current) {
-        ref.current.style.backgroundImage = `linear-gradient(${degree}deg, var(--color-primary-light), var(--color-secondary-light))`
-      }
-    }, 100)
-    return (): void => clearInterval(interval)
-  })
 
   return (
     <span
       ref={ref}
       className={clsx(
-        'from-primary-light to-secondary-light bg-clip-text',
+        'bg-gradient-light bg-clip-text',
         'transition text-transparent',
         className
       )}
