43225085be284dcbc1ba1afa6f0c77c53277db5b
Jose Stephen <jose.satrya@gmail.com>
2025-03-22 18:18:43 +0700

    feat: update HeadingOne size breakpoint

--- src/components/common/HeadingOne.tsx
+++ src/components/common/HeadingOne.tsx
@@ -4,7 +4,7 @@ import { FC } from 'react'
 
 const HeadingOne: FC<PageHeadingProps> = ({ children, className }) => {
   return (
-    <h1 className={clsx('font-bold md:text-4xl text-3xl', className)}>
+    <h1 className={clsx('font-bold sm:text-4xl text-3xl', className)}>
       {children}
     </h1>
   )
